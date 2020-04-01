087d6a71aabb70853eafbef9206294e13022cfae
Theodior <theo0230@stud.kea.dk>
2020-04-01 15:50:26 +0200

    Full comit of whole website

---
+++ script.js
@@ -0,0 +1,38 @@
+$(window).on('load', function () {
+    console.log("pre");
+    $('.preloader').fadeOut('slow');
+});
+
+(function ($) {
+    $(window).on('load', function () {
+        // https://github.com/jlmakes/scrollreveal
+        ScrollReveal().reveal('.uno');
+        ScrollReveal().reveal('.twoo');
+        ScrollReveal().reveal('.thr33');
+        ScrollReveal().reveal('.four');
+        ScrollReveal().reveal('.five');
+        ScrollReveal().reveal('.six');
+        ScrollReveal().reveal('.seven');
+        ScrollReveal().reveal('.eight');
+        ScrollReveal().reveal('.nine');
+        ScrollReveal().reveal('.ten');
+        ScrollReveal().reveal('.eleven');
+        ScrollReveal().reveal('.twelve');
+        ScrollReveal().reveal('.thirteen');
+        ScrollReveal().reveal('.fourteen');
+        ScrollReveal().reveal('.fitfhteen');
+        ScrollReveal().reveal('.sixteen');
+        ScrollReveal().reveal('.seventeen');
+        ScrollReveal().reveal('.eigthteen');
+        ScrollReveal().reveal('.nineteen');
+        ScrollReveal().reveal('.twenty');
+        ScrollReveal().reveal('.twentyone');
+        ScrollReveal().reveal('.twentytwo');
+        ScrollReveal().reveal('.twentythree');
+        ScrollReveal().reveal('.twentyfour');
+        ScrollReveal().reveal('.twentyfive');
+        ScrollReveal().reveal('.twentysix');
+        ScrollReveal().reveal('.twentyseven');
+        ScrollReveal().reveal('.twentyeight');
+    });
+})(jQuery);
